ba22341ab812577baf4b6984bb5d796483d7db8e
Mario Pérez Esteso <mario@geekytheory.com>
2020-12-07 12:26:30 +0100

    feat(chartData): transform JSON data for linear chart

---
+++ src/utils/LeaderBoardJsonTransformer.js
@@ -0,0 +1,82 @@
+const LeaderBoardJsonTransformer = (apiJsonResponse) => {
+    let chartData = {
+        labels: [],
+        datasets: []
+    }
+
+    let leaderBoardMembers = Object.entries(apiJsonResponse['members'])
+
+    if (leaderBoardMembers.length == 0) {
+        return chartData;
+    }
+
+    let leaderBoardByDay = {}
+
+    leaderBoardMembers.forEach(([userId, userData]) => {
+        Object.entries(userData['completion_day_level']).forEach(([day, problemsSolved]) => {
+            if (day == '1') {
+                // Day one is skipped in the leaderboard by Advent of Code organization
+                return;
+            }
+
+            if (!(day in leaderBoardByDay)) {
+                leaderBoardByDay[day] = {}
+                leaderBoardByDay[day]['1'] = {}
+                leaderBoardByDay[day]['2'] = {}
+            }
+
+            ['1', '2'].forEach(problemNumber => {
+                if (problemNumber in problemsSolved) {
+                    leaderBoardByDay[day][problemNumber][problemsSolved[problemNumber]['get_star_ts']] = userId;
+                }
+            })
+        })
+    })
+
+    let userPointsByDay = {};
+
+    // Initialize user points by day to zero. This way, if a user has not solved a problem of a day, it
+    // get zero points by default and it has not missing data.
+    leaderBoardMembers.forEach(([userId, userData]) => {
+        userPointsByDay[userId] = {};
+        Object.keys(leaderBoardByDay).sort().forEach(day => {
+            userPointsByDay[userId][day] = 0;
+        })
+    })
+
+    // Calculate user points by day
+    Object.entries(leaderBoardByDay).forEach(([day, problems]) => {
+        ['1', '2'].forEach(problemNumber => {
+            let pointsForUser = leaderBoardMembers.length;
+            Object.keys(problems[problemNumber]).sort().forEach(function (timestamp) {
+                let userId = problems[problemNumber][timestamp];
+
+                userPointsByDay[userId][day] += pointsForUser--;
+            })
+        })
+    })
+
+    // Build dataset
+    chartData.labels = Object.keys(leaderBoardByDay)
+
+    Object.entries(userPointsByDay).forEach(([userId, pointsByDay]) => {
+        let dataset = {
+            'label': apiJsonResponse['members'][userId]['name'],
+            'data': []
+        }
+
+        Object.keys(pointsByDay).sort().forEach(day => {
+            if (day > 2) {
+                dataset.data.push(pointsByDay[day] + dataset.data[dataset.data.length - 1]);
+            } else {
+                dataset.data.push(pointsByDay[day]);
+            }
+        })
+
+        chartData.datasets.push(dataset);
+    })
+
+    return chartData
+}
+
+module.exports = LeaderBoardJsonTransformer
---
+++ src/utils/LeaderBoardJsonTransformer.test.js
@@ -0,0 +1,138 @@
+const LeaderBoardJsonTransformer = require('./LeaderBoardJsonTransformer');
+
+
+test('Transform empty leaderboard', () => {
+  let jsonData = {
+    "event": "2020",
+    "members": {},
+    "owner_id": "123456"
+  }
+
+  let result = LeaderBoardJsonTransformer(jsonData)
+
+  expect(result).toEqual({
+    labels: [],
+    datasets: []
+  });
+});
+
+
+test('Transform leaderboard with a single member that has not resolved any problem', () => {
+  let jsonData = {
+    "event": "2020",
+    "members": {
+      "123456": {
+        "global_score": 0,
+        "stars": 0,
+        "id": "123456",
+        "local_score": 0,
+        "last_star_ts": 0,
+        "completion_day_level": {},
+        "name": "Pepito"
+      }
+    },
+    "owner_id": "123456"
+  }
+
+  let result = LeaderBoardJsonTransformer(jsonData)
+
+  expect(result).toEqual({
+    labels: [],
+    datasets: [
+      {
+        label: 'Pepito',
+        data: []
+      }
+    ]
+  });
+})
+
+test('Transform leaderboard with multiple members', () => {
+  let jsonData = {
+    "members": {
+      "1234567": {
+        "local_score": 25,
+        "stars": 12,
+        "name": "Javi",
+        "id": "1234567",
+        "last_star_ts": "1607243081",
+        "global_score": 0,
+        "completion_day_level": {
+          "1": {
+            "1": {
+              "get_star_ts": "10"
+            },
+            "2": {
+              "get_star_ts": "35"
+            }
+          },
+          "3": {
+            "1": {
+              "get_star_ts": "1030"
+            }
+          },
+          "2": {
+            "1": {
+              "get_star_ts": "120"
+            },
+            "2": {
+              "get_star_ts": "155"
+            }
+          },
+        }
+      },
+      "9876543": {
+        "local_score": 25,
+        "name": "Mario",
+        "stars": 12,
+        "last_star_ts": "1607245811",
+        "id": "9876543",
+        "global_score": 0,
+        "completion_day_level": {
+          "3": {
+            "1": {
+              "get_star_ts": "1050"
+            },
+            "2": {
+              "get_star_ts": "1060"
+            }
+          },
+          "1": {
+            "1": {
+              "get_star_ts": "15"
+            },
+            "2": {
+              "get_star_ts": "25"
+            }
+          },
+          "2": {
+            "1": {
+              "get_star_ts": "110"
+            },
+            "2": {
+              "get_star_ts": "130"
+            }
+          }
+        }
+      }
+    },
+    "owner_id": "9876543",
+    "event": "2020"
+  }
+
+  let result = LeaderBoardJsonTransformer(jsonData)
+
+  expect(result).toEqual({
+    labels: ['2', '3'],
+    datasets: [
+      {
+        label: 'Javi',
+        data: [2, 4]
+      },
+      {
+        label: 'Mario',
+        data: [4, 7]
+      }
+    ]
+  });
+})
