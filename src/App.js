d0cd28657f5646d10f4377480d0b8af3a1fb7c8f
Mario Pérez Esteso <mario@geekytheory.com>
2020-12-08 13:10:53 +0100

    feat(chart): create input for chart data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+import { LineChart, DataTextArea } from './components'
 import './App.css';
-import LineChart from './components/LineChart';
+const leaderBoardJsonTransformer = require('./utils/LeaderBoardJsonTransformer');
 
-function App() {
+const App = () => {
+  const [linearChartData, setLinearChartData] = useState({})
+
+  const handleData = (parameter) => {
+    setLinearChartData(leaderBoardJsonTransformer(parameter))
+  }
+
   return (
     <div className="App">
-      <div className='chart'>
-        <LineChart />
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-3'>
+            <DataTextArea dataHandler={handleData} />
+          </div>
+          <div className='col-9'>
+            <div className='chart'>
+              <LineChart data={linearChartData} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
